8ef3df9e180ab820a055e7e3127c06f11b3bf647
Lexx R <alexcumbal@gmail.com>
2023-07-28 16:11:49 -0500

    General Arquitechture

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Header from "./common/Header";
+import { Footer } from "./common/Footer";
+import Home from "./pages/Home";
+
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
       
     </>
   )
---
+++ src/theme/GlobalStyle.tsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body, main, div, #root {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    background-color: #E5E5E5;
+    font-family: 'DM Sans', sans-serif;
+    font-size: 16px;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyle;
